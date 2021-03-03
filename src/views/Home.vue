d8063e10659427f5f6733a7bd8ed87e07fef87b5
m.morshinin <m.morshinin@b2b-center.ru>
2021-03-04 01:38:31 +0300

    Create a search form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,10 @@
         </div>
       </router-link>
     </div>
+    <form @submit.prevent="" class="search-box">
+      <input type="text" placeholder="Search something...">
+      <input type="submit" value="Search">
+    </form>
   </div>
 </template>
 
