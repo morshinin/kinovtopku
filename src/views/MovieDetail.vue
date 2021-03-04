758d68d7fc2e33bddc8a47f41dec530bb39bc08f
m.morshinin <m.morshinin@b2b-center.ru>
2021-03-04 23:39:42 +0300

    finish single movie page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,15 +1,64 @@
 <template>
   <div class="movie-detail">
-    Detail {{ $route.params.id }}
+    <h2>
+      {{ movie.Title }}
+    </h2>
+    <p>
+      {{ movie.Year }}
+    </p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img">
+    <p>
+      {{ movie.Plot }}
+    </p>
   </div>
 </template>
 
 <script>
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from '@/env';
+
 export default {
-name: "MovieDetail"
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(`http://omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+      .then(response => response.json())
+      .then(data => {
+        movie.value = data;
+      })
+    })
+
+    return {
+      movie
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.movie-detail {
+  padding: 16px;
 
+  h2 {
+    color: #FFFFFF;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+
+  .featured-img {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    color: #FFFFFF;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
 </style>
