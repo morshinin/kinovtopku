98d96d052f20fd742f6102f8feff7b6d7c6dcf10
m.morshinin <m.morshinin@b2b-center.ru>
2021-03-08 05:20:21 +0300

    new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-// import MovieDetail from '../views/MovieDetail.vue'
-import NewHome from "@/views/NewHome";
+import Home from "@/views/Home";
 import SingleMovie from "@/views/SingleMovie";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
   {
     path: '/movie/:id',
     name: 'Single Movie',
     component: SingleMovie
   },
   {
-    path: '/new-home',
-    name: 'New Home',
-    component: NewHome
+    path: '/',
+    name: 'Home',
+    component: Home
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,212 +1,51 @@
 <template>
-  <div class="home">
-    <div class="feature-card">
-      <router-link to="/movie/tt0409591">
-        <img src="http://img.omdbapi.com/?apikey=1497dc5a&i=tt0409591" alt="Naruto Poster"
-        class="feature-img">
-        <div class="detail">
-          <h3>Naruto</h3>
-          <p>
-            Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming the Hokage, the village's leader and strongest ninja.
-          </p>
-        </div>
-      </router-link>
-    </div>
-    <form @submit.prevent="SearchMovies()" class="search-box">
-      <input type="text" placeholder="Search something..." v-model="search">
-      <input type="submit" value="Search">
-    </form>
-    <div class="movies-list">
-      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
-        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
-          <div class="product-image">
-            <img :src="movie.Poster" alt="Movie Poster">
-            <div class="type">
-              {{ movie.Type }}
+  <div>
+    <div class="container" v-for="(movie, index) in movies" :key="movie.id" :index="index">
+      <div class="row my-2">
+        <article class="col">
+          <router-link :to="'/movie/' + movie.id">
+          <div class="card flex-row">
+            <div class="card-header">{{ index + 1 }}</div>
+            <img class="card-img-left" :src="'http://image.tmdb.org/t/p/w200' + movie.poster_path"
+                 v-if="movie.poster_path !== null" alt="Card image cap"
+            width="200" height="300">
+            <div class="card-body">
+              <h1 class="card-title">{{ movie.title }}</h1>
+              <p><small>{{ movie.release_date }}</small></p>
+              <p class="card-text" v-if="movie.overview">{{ movie.overview }}</p>
             </div>
           </div>
-          <div class="movie-detail">
-            <p class="year">{{ movie.Year }}</p>
-            <h3>{{ movie.Title }}</h3>
-          </div>
-        </router-link>
+          </router-link>
+        </article>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import env from '@/env.js';
+import { ref, onBeforeMount } from 'vue';
+import env from '@/env';
 
 export default {
   setup() {
-    const search = ref("");
     const movies = ref([]);
 
-    const SearchMovies = () => {
-      if (search.value !== "") {
-        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+    onBeforeMount(() => {
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${env.apikey}&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1`)
         .then(response => response.json())
         .then(data => {
-          movies.value = data.Search;
-          search.value = '';
+          movies.value = data.results;
+          console.log(movies.value);
         })
-      }
-    }
+    })
 
     return {
-      search,
-      movies,
-      SearchMovies
+      movies
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-  .home {
-    .feature {
-      &-card {
-        position: relative;
-      }
+<style scoped>
 
-      &-img {
-        display: block;
-        width: 100%;
-        height: 300px;
-        object-fit: cover;
-        position: relative;
-        z-index: 0;
-      }
-    }
-
-    .detail {
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background-color: rgba(0, 0, 0, .6b);
-      padding: 16px;
-      z-index: 1;
-
-      h3 {
-        color: #FFFFFF;
-        margin-bottom: 16px;
-      }
-
-      p {
-        color: #FFFFFF;
-      }
-    }
-
-    .search-box {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 16px;
-
-      input {
-        display: block;
-        appearance: none;
-        border: none;
-        outline: none;
-        background: none;
-
-        &[type="text"] {
-          width: 100%;
-          color: #FFFFFF;
-          background-color: #496583;
-          font-size: 20px;
-          padding: 10px 16px;
-          border-radius: 8px;
-          margin-bottom: 15px;
-          transition: .4s;
-
-          &::placeholder {
-            color: #F3F3F3;
-          }
-
-          &:focus {
-            box-shadow: 0 3px 6px rgba(0, 0, 0, .2);
-          }
-        }
-
-        &[type="submit"] {
-          width: 100%;
-          max-width: 300px;
-          background-color: #42B883;
-          padding: 16px;
-          border-radius: 8px;
-          color: #FFFFFF;
-          font-size: 20px;
-          text-transform: uppercase;
-          transition: .4s;
-          
-          &:active {
-            background-color: #3B8070;
-          }
-        }
-      }
-    }
-
-    .movies-list {
-      display: flex;
-      flex-wrap: wrap;
-      margin: 0 8px;
-
-      .movie {
-        max-width: 50%;
-        flex: 1 1 50%;
-        padding: 16px 8px;
-
-        &-link {
-          display: flex;
-          flex-direction: column;
-          height: 100%;
-
-          .product-image {
-            position: relative;
-            display: block;
-            
-            img {
-              display: block;
-              width: 100%;
-              height: 275px;
-              object-fit: cover;
-            }
-
-            .type {
-              position: absolute;
-              padding: 8px 16px;
-              background-color: #42B883;
-              color: #FFFFFF;
-              bottom: 16px;
-              left: 0;
-              text-transform: capitalize;
-            }
-          }
-
-          .movie-detail {
-            background-color: #496583;
-            padding: 16px 8px;
-            flex: 1 1 100%;
-            border-radius: 0 0 8px 8px;
-
-            .year {
-              color: #AAAAAA;
-              font-size: 14px;
-            }
-
-            h3 {
-              color: #FFFFFF;
-              font-weight: 600;
-              font-size: 18px;
-            }
-          }
-        }
-      }
-    }
-  }
 </style>
